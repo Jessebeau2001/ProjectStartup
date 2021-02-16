288afa0c2440bbd8fcf5eea917df08ef4883906c
Tom de Kinkelder <tomdekinkelder@live.nl>
2021-02-16 11:59:54 +0100

    Added Setting Time for alarm

--- Assets/Scripts/SetAlarm.cs
+++ Assets/Scripts/SetAlarm.cs
@@ -7,20 +7,27 @@ public class SetAlarm : MonoBehaviour
     const string ACTION_SET_ALARM = "android.intent.action.SET_ALARM";
     const string EXTRA_HOUR = "android.intent.extra.alarm.HOUR";
     const string EXTRA_MINUTES = "android.intent.extra.alarm.MINUTES";
+    const string EXTRA_DAYS = "android.intent.extra.alarm.DAYS";
     const string EXTRA_MESSAGE = "android.intent.extra.alarm.MESSAGE";
+
+    string AlarmTitle = "Test";
+    int AlarmMinutes = 00;
+    int AlarmHours = 12;
+    int AlarmDay = 1;
 
     public void OnClick()
     {
-        Debug.Log("Setting Alarm");
-        createAlarm("TestAlarm", 11, 00);
+        Debug.Log("Setting Alarm: " + AlarmHours + ":" + AlarmMinutes + " " + AlarmTitle);
+        createAlarm();
     }
 
-    public void createAlarm(string message = "Test", int hours = 12, int minutes = 00)
+    public void createAlarm()
     {
         var intentAJO = new AndroidJavaObject("android.content.Intent", ACTION_SET_ALARM);
-        intentAJO.Call<AndroidJavaObject>("putExtra", EXTRA_MESSAGE, message)
-            .Call<AndroidJavaObject>("putExtra", EXTRA_MINUTES, minutes)
-            .Call<AndroidJavaObject>("putExtra", EXTRA_HOUR, hours);
+        intentAJO.Call<AndroidJavaObject>("putExtra", EXTRA_MESSAGE, AlarmTitle)
+            .Call<AndroidJavaObject>("putExtra", EXTRA_MINUTES, AlarmMinutes)
+            .Call<AndroidJavaObject>("putExtra", EXTRA_HOUR, AlarmHours)
+            .Call<AndroidJavaObject>("putExtra", EXTRA_DAYS, AlarmDay);
         GetUnityActivity().Call("startActivity", intentAJO);
 
     }
@@ -31,5 +38,26 @@ public class SetAlarm : MonoBehaviour
         {
             return unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
         }
+    }
+
+
+    public void SetAlarmTitle(string title)
+    {
+        AlarmTitle = title;
+    }
+
+    public void SetAlarmMinutes(int minutes)
+    {
+        AlarmMinutes = minutes;
+    }
+
+    public void SetAlarmHours(int hours)
+    {
+        AlarmHours = hours;
+    }
+
+    public void SetAlarmDay(int day)
+    {
+        AlarmDay = day;
     }
 }
